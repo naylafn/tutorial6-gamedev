57fa75ff06d9a384a3829e92a4dbf3d5946c0077
naylafn <naylafarah.31@gmail.com>
2026-03-25 01:41:38 +0700

    fix: reset global lives variable after game over

--- scenes/LinkButton.gd
+++ scenes/LinkButton.gd
@@ -7,19 +7,21 @@ func _on_New_Game_pressed():
 	get_tree().change_scene_to_file(str("res://scenes/" + scene_to_load + ".tscn"))
 
 func _on_Try_Again_pressed():
+	Global.lives += 3
 	get_tree().change_scene_to_file(str("res://scenes/" + scene_to_load + ".tscn"))
 
 func _on_Main_Menu_pressed():
+	Global.lives += 3
 	get_tree().change_scene_to_file(str("res://scenes/" + "MainMenu" + ".tscn"))
 
 func _on_Stage_pressed():
 	get_tree().change_scene_to_file(str("res://scenes/Stage.tscn"))
 
 func _on_Level_1_pressed():
-	get_tree().change_scene_to_file(str("res://scenes/" + scene_to_load + ".tscn"))
+	get_tree().change_scene_to_file(str("res://scenes/" + "Level 1" + ".tscn"))
 
 func _on_Level_2_pressed():
-	get_tree().change_scene_to_file(str("res://scenes/" + scene_to_load + ".tscn"))
+	get_tree().change_scene_to_file(str("res://scenes/" + "Level 2" + ".tscn"))
 
 func _on_Exit_pressed():
 	get_tree().paused = false
