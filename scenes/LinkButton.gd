e8ab429e2ffbd1c3d0abfa025e34fbccb6ab9ff3
naylafn <naylafarah.31@gmail.com>
2026-03-24 21:22:30 +0700

    Latihan Mandiri: tombol try again dan kembali ke main menu di layar game over

--- scenes/LinkButton.gd
+++ scenes/LinkButton.gd
@@ -4,3 +4,9 @@ extends LinkButton
 
 func _on_New_Game_pressed():
 	get_tree().change_scene_to_file(str("res://scenes/" + scene_to_load + ".tscn"))
+
+func _on_Try_Again_pressed():
+	get_tree().change_scene_to_file(str("res://scenes/" + scene_to_load + ".tscn"))
+
+func _on_Main_Menu_pressed():
+	get_tree().change_scene_to_file(str("res://scenes/" + "MainMenu" + ".tscn"))
